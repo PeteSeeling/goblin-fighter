457f58e23778ad0b67db7a8bc1a7d712410f6be3
Pete Seeling <peteseeling@gmail.com>
2021-12-10 14:09:12 -0500

    html Done. form evnt. listner working

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 // import functions and grab DOM elements
+const adventurerDataEl = document.getElementById('adventurer-data');
+const defeatedNumberEl = document.getElementById('defeated-number');
+const defeatedListEl = document.getElementById('defeat-list');
+const adventurerHpEl = document.getElementById('adventurer-hp');
+const form = document.querySelector('form');
+const goblinsEl = document.getElementById('goblins');
 
 // let state
+let defeatedNumber = 0;
+let adventurerHp = 5;
+
+let goblins = [
+    { name: 'zeus', hp:3 },
+    { name: 'al', hp:2 },
+];
 
 // set event listeners 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+    const goblinName = data.get('goblin-name');
+
+    const newGoblin = {
+        name: goblinName,
+        hp: Math.floor(Math.random() * 3),
+    };
+    goblins.push(newGoblin);
+
+    console.log(newGoblin);
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
